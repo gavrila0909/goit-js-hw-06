7b14f8606b3a9315a484bedb9d6793864e3cca22
gavrila0909 <gavriladanna@gmail.com>
2024-01-11 10:48:41 +0000

    done task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,19 @@
-const textInput = document.getElementById('validation-input');
-textInput.addEventListener( 'blur', () => {
-    const expectedLength = parseInt(textInput.getAttribute('data-length'), 10);
-    const currentLength = textInput.value.trim().length;
 
+const textInput = document.getElementById('validation-input');
+
+
+textInput.addEventListener( 'blur', () => {
+    const expectedLength = Number(textInput.getAttribute('data-length'));
+    const currentLength = textInput.value.trim().length;
     if (currentLength === expectedLength) {
-      textInput.classList.remove('invalid');
       textInput.classList.add('valid');
+      textInput.classList.remove('invalid');
     } else {
-      textInput.classList.remove('valid');
       textInput.classList.add('invalid');
+      textInput.classList.remove('valid');
     }
   });
+
+
 
 
