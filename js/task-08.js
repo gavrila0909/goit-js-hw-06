a200aa791f0613f6dd09b261c82091829b524e4c
gavrila0909 <gavriladanna@gmail.com>
2024-01-13 11:43:34 +0000

    done task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
 
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', e => {
+e.preventDefault();
+
+const email = document.querySelector('input[type="email"]');
+const password = document.querySelector('input[type="password"]');
+
+
+if(email.value === '' || password.value === ''){
+   alert('Te rugam sa introduci o valoare in ambele campuri!');
+} else {
+    console.log('Email:', email.value );
+    console.log('Password:', password.value);
+    loginForm.reset();
+    alert('Formularul a fost salvat cu succes');
+}
+
+ })
