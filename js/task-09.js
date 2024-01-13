0c8ac367d5445bebdeb4d3ff997f3985418ba5dc
gavrila0909 <gavriladanna@gmail.com>
2024-01-13 13:26:47 +0000

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
-function getRandomHexColor() {
+
+ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector('.change-color');
+const spanValue = document.querySelector('.color')
+
+ button.addEventListener ('click', () => {
+
+   spanValue.textContent = getRandomHexColor();
+   button.style.backgroundColor = getRandomHexColor();
+   document.body.style.backgroundColor = getRandomHexColor();
+ }) 
+ 
